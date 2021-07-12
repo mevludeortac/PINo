6eb842d822c03786316d78305924edfebaf04766
mevludeortac <ortacmevlude@gmail.com>
2021-07-12 17:08:27 +0300

    Initial Commit

---
+++ PINo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PINo
+//
+//  Created by MEWO on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
