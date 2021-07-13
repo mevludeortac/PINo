9c05046346849e6f89a7a160b271e7e48f44e41c
mevludeortac <ortacmevlude@gmail.com>
2021-07-13 19:01:03 +0300

    first commit

---
+++ PINo/TableViewController.swift
@@ -0,0 +1,70 @@
+//
+//  TableViewController.swift
+//  PINo
+//
+//  Created by MEWO on 13.07.2021.
+//
+
+import UIKit
+import CoreData
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    var titleArray = [String]()
+    var idArray = [UUID]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        getData()
+
+    }
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titleArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = titleArray[indexPath.row]
+        return cell
+    }
+
+    
+    func getData(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        request.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject]{
+                    if let title = result.value(forKey: "title") as? String {
+                        self.titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        self.idArray.append(id)
+                    }
+                    tableView.reloadData()
+                }
+                            }
+        }catch{
+            print("error")
+        }
+        
+    }
+}
--- PINo/ViewController.swift
+++ PINo/ViewController.swift
@@ -6,14 +6,85 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var locatioNameTxt: UITextField!
+    @IBOutlet weak var commenTxt: UITextField!
+    let locationManager = CLLocationManager()
+    
+    //seçilen konumun enlem ve boylamını almak için
+    var choosenLatitude = Double()
+    var choosenLongitude = Double()
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //konum keskinliği
+        //kullanıcıdan konum izni alma
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureRecognizer)
+                                            
+    }
+    
+    @objc func chooseLocation(gestureRecognizer:UILongPressGestureRecognizer){
+        if gestureRecognizer.state == .began
+        {
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            choosenLongitude = touchedCoordinates.longitude
+            choosenLatitude = touchedCoordinates.latitude
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = touchedCoordinates
+            annotation.title = locatioNameTxt.text!
+            annotation.subtitle = commenTxt.text!
+            mapView.addAnnotation(annotation)
+            
+        }
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //enlem boylam
+        let locations = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        //zoom işlemi
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        
+        //belirtilen enlem ve boylamı zoomlayacak harita
+        let region = MKCoordinateRegion(center: locations, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        newPlace.setValue(locatioNameTxt.text, forKey: "title")
+        newPlace.setValue(commenTxt.text, forKey: "subtitle")
+        newPlace.setValue(choosenLatitude, forKey: "latitude")
+        newPlace.setValue(choosenLongitude, forKey: "longitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do{
+            try context.save()
+            print("kaydedildi")
+            
+        }catch{
+            print("error")
+        }
+        
+    }
 }
 
